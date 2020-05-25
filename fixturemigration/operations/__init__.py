bbae315932b6bff675c75b970f94df157ca07a16
John Vandenberg <jayvdb@gmail.com>
2020-05-25 14:25:28 +0700

    Allow permissions
    c.f. https://code.djangoproject.com/ticket/23422

--- fixturemigration/operations/__init__.py
+++ fixturemigration/operations/__init__.py
@@ -1,6 +1,7 @@
 from django.db import router
 from django.db.migrations.operations.base import Operation
 from django.core.management import call_command
+from django.core.management.sql import emit_post_migrate_signal
 
 
 class LoadFixtureMigration(Operation):
@@ -14,6 +15,7 @@ class LoadFixtureMigration(Operation):
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if router.allow_migrate(schema_editor.connection.alias, app_label):
+            emit_post_migrate_signal(2, False, 'default')
             call_command('load_fixturemigration', self.fixturemigration, state=from_state)
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
